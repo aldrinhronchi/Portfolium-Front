7d5c2cca386b5fec59a7417d8e9f50b63159063d
Terore <aldrinhronchi@gmail.com>
2023-02-26 00:49:03 -0300

    Feat: Adicionado bootstrap, pagina Home para teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterBarComponent } from './footer-bar/footer-bar.component';
